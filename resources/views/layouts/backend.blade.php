2b00226976c50b9bd204909ec61edeb3ea2af4cc
aldiui <aldijaya280902@gmail.com>
2024-05-27 18:55:22 +0700

    updated data

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -3,7 +3,8 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <title>@yield('title') - {{ config('app.name') }}</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="shortcut icon" href="{{ asset('backend/compiled/svg/favicon.svg') }}" type="image/x-icon">
